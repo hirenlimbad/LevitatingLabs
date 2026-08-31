c8e09f062b3fef4f216bafb9d3d72801951286e1
hirenlimbad <limbadhiren00@gmail.com>
2026-08-31 16:35:01 +0530

    refactor: change post URL routing from /posts/ to /articles/

--- src/utils/getPostPaths.ts
+++ src/utils/getPostPaths.ts
@@ -48,5 +48,5 @@ export function getPostUrl(
   filePath: string | undefined,
   locale: string | undefined = config.site.lang
 ): string {
-  return getRelativeLocaleUrl(locale, `posts/${getPostSlugPath(id, filePath)}`);
+  return getRelativeLocaleUrl(locale, `articles/${getPostSlugPath(id, filePath)}`);
 }
